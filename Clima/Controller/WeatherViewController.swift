52890812ef8c18bfeba088dc53ca9e0f445084bd
KrasnovYuri <krasnov1yuri@gmail.com>
2023-11-22 11:28:52 +0300

    add IBOutlets

---
+++ Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Юрий on 22.11.2023.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
